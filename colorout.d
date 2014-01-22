3f202ad9c794d5063fff988007a7deb4d3177ba7
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-01-22 21:04:40 +0000

    Add --locations for parsed error locations, various tweaks

--- colorout.d
+++ colorout.d
@@ -1,6 +1,28 @@
 module colorout;
 
+/++
+	Color file syntax:
+
+	Each line has the structure: <COLOR><TAB><REGEX>
+	- <COLOR> is the console attribute to assign to
+	  the line, if <REGEX> matches. Windows console
+	  attributes are a superset of VGA color codes,
+	  for more information see:
+	  http://msdn.microsoft.com/en-us/library/windows/desktop/ms682088(v=vs.85).aspx#_win32_character_attributes
+	  If <COLOR> is 0x00, only regex named groups are
+	  processed, the attribute is not applied, and
+	  the search continues.
+	  If <COLOR> is 0x11, the line is omitted from
+	  the output completely.
+	- <REGEX> is a regular expression matched against
+	  each output line.
+	  If it contains the named groups <file> and
+	  <line>, these are written to the file specified
+	  using the --locations command-line parameter.
+++/
+
 import std.c.windows.windows;
+import std.algorithm;
 import std.conv;
 import std.exception;
 import std.getopt;
@@ -13,12 +35,15 @@ import std.string;
 int main(string[] args)
 {
 	enforce(args.length >= 3,
-		"Usage: " ~ args[0] ~ " RULES.col PROGRAM [ARGS...]");
+		"Usage: " ~ args[0] ~ " RULES.col [--maxlines=N] [--locations=FILENAME] PROGRAM [ARGS...]");
 	
 	int lines = int.max;
+	string locationsFileName;
 	getopt(args,
 		config.stopOnFirstNonOption,
-		"maxlines", &lines);
+		"maxlines", &lines,
+		"locations", &locationsFileName,
+	);
 
 	struct Rule
 	{
@@ -30,7 +55,7 @@ int main(string[] args)
 		if (line.length)
 		{
 			auto segs = line.strip().split("\t");
-			rules ~= Rule(parse!(ushort)(segs[0], 16), regex(segs[1]));
+			rules ~= Rule(parse!(ushort)(segs[0], 16), regex(segs[1].idup));
 		}
 
 	auto p = pipe();
@@ -39,22 +64,56 @@ int main(string[] args)
 
 	auto h = GetStdHandle(STD_OUTPUT_HANDLE);
 
+	File locations;
+	if (locationsFileName)
+		locations.open(locationsFileName, "wb");
+
+lineLoop:
 	foreach (line; p.readEnd.byLine())
 	{
-		lines--;
-		if (lines >= 0)
+		line = line.chomp();
+		ushort attr = 7;
+		foreach (ref rule; rules)
 		{
-			ushort attr = 7;
-			foreach (ref rule; rules)
-				if (!match(line, rule.r).empty)
+			auto m = match(line, rule.r);
+			if (m)
+			{
+				auto names = rule.r.namedCaptures;
+				if (locationsFileName && names.canFind("file") && names.canFind("line"))
+				{
+					auto fields =
+					[
+						m.captures["file"],
+						m.captures["line"],
+						names.canFind("column" ) ? m.captures["column" ] : "",
+						names.canFind("message") ? m.captures["message"] : line,
+					];
+					locations.writeln(fields.join("\t"));
+				}
+
+				if (rule.attr == 0x00)
+					continue;
+				else
+				if (rule.attr == 0x11)
+					//continue lineLoop; // https://d.puremagic.com/issues/show_bug.cgi?id=11885
+					goto nextLine;
+				else
 				{
 					attr = rule.attr;
 					break;
 				}
+			}
+		}
+
+		lines--;
+		if (lines >= 0)
+		{
 			SetConsoleTextAttribute(h, attr);
 			stderr.writeln(line);
 			SetConsoleTextAttribute(h, 7);
 		}
+
+	nextLine:
 	}
 	if (lines < 0)
 		writefln("( ... %d lines omitted ... )", -lines);
